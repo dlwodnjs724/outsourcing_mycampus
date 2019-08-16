74a81de17de3aa6da5f2e2844d98dfb749d47ba2
yongsk0066 <yongsk0066@naver.com>
2019-08-16 16:25:46 +0900

    suggested category model 추가

--- project/board/admin.py
+++ project/board/admin.py
@@ -3,19 +3,38 @@ from .models import Category,Post,Image,Comment
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_per_page = 10
+    list_per_page = 20
     list_display = (
         'id', 'title', 'ctgy', 'user', 'created_at',
         'likes_count','views', 'is_anonymous'
     )
-    list_filter = ('ctgy',)
     search_fields = ('title',)
     ordering = ('-created_at', 'ctgy')
 
     def likes_count(self, obj):
         return obj.likes.count()
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = (
+        'id', 'univ', 'name', 'created_at', 'status', 'post_count'
+    )
+    search_fields = ('name', 'univ', 'status')
+    ordering = ('created_at', )
+
+    def post_count(self, obj):
+        return Post.objects.filter(ctgy=obj).count()
+
+class CommentAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = (
+        'id', 'post', 'parent', 'content','author', 'created_at', 'is_anonymous', 'likes_count', 
+    )
+
+    def likes_count(self, obj):
+        return obj.comment_likes.count()
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Image)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
--- project/board/models.py
+++ project/board/models.py
@@ -4,6 +4,7 @@ from datetime import datetime, timezone
 from accounts.models import User
 from core.models import Univ
 
+
 class Category(models.Model):
     univ = models.ForeignKey(Univ, on_delete=models.CASCADE, related_name='category',blank=False)
     name = models.CharField(max_length=30, blank=False)
@@ -17,13 +18,17 @@ class Category(models.Model):
     def __str__(self):
         return f'Category (PK: {self.pk}, Name: {self.name}, Univ: {self.univ.name})'
 
-# def get_short_title(e):
-#     if len(" ".join(e.split()[0:2])) > 10:
-#         a = " ".join(e.split()[0:1].append("..."))
-#         return str(a)
-#     else:
-#         a = " ".join(e.split()[0:2])
-#         return str(a)
+
+class Suggested(models.Model):
+    univ = models.ForeignKey(Univ, on_delete=models.CASCADE, related_name='suggested_category',blank=False)
+    name = models.CharField(max_length=30, blank=False)
+    dscrp = models.TextField(blank=False)
+    suggested_at = models.DateTimeField(auto_now_add=True)
+    suggested_by = models.ForeignKey(User, on_deleted=models.CASCADE)
+
+def __str__(self):
+    return f'Suggested_Category: {self.name}'
+
 
 class Post(models.Model):
     ctgy = models.ForeignKey(Category, on_delete=models.CASCADE, related_name ='posts',blank=False)
@@ -33,7 +38,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
 
-    likes = models.ManyToManyField(User,default=0, related_name='liked', blank=True)
     views = models.PositiveIntegerField(default=0)
     is_anonymous = models.BooleanField(default=True)
 
@@ -52,6 +56,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f'Post (PK: {self.pk}, Title: {" ".join(self.title.split()[0:2])}... Author: {self.user.username})'
+
 
 def get_image_filename(instance, filename):
     id = instance.post.id
@@ -88,5 +93,5 @@ class Comment(models.Model):
     def total_likes(self):
         return self.comment_likes.count()
 
-    def __str__(self):
-        return f'Comment (PK: {self.pk}, Author: {self.author.username} Parent: {self.parent})'
+    # def __str__(self):
+    #     return f'Comment (PK: {self.pk}, Author: {self.author.username} Parent: {self.parent})'
