f401ad88cf1e82fa7899ce2fd8058b3b870b17d3
EatNug <wlq4568@gmail.com>
2019-08-17 20:57:56 +0900

    signup validation view

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -2,3 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = "accounts"
+
+urlpatterns = [
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+]
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -9,11 +9,11 @@ from .models import User
 # Create your views here.
 from django.views.generic.base import View
 
-
+# reqeust, User 모델 인스턴스, 보낼 이메일 주소
 def sendMail(request, user, to_email):
     current_site = get_current_site(request)
     mail_subject = 'Activate your blog account.'
-    message = render_to_string('activationMail.html', {
+    message = render_to_string('registration/activationMail.html',{
         'user': user,
         'domain': current_site.domain,
         'uid':urlsafe_base64_encode(force_bytes(user.pk)),
@@ -24,13 +24,13 @@ def sendMail(request, user, to_email):
     )
     email.send()
 
-
+#uid, token
 def activate(request, uidb64, token):
     uid = force_text(urlsafe_base64_decode(uidb64))
     user = get_object_or_404(User, pk=uid)
 
     if account_activation_token.check_token(user, token):
-        user.is_active = True
+        user.is_validate = True
         user.save()
         login(request, user)
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
