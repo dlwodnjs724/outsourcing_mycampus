953932afddbf7b35aec8c7c8fe46baf10275e430
이재원 <dlwodnjs724@naver.com>
2019-08-17 22:57:13 +0900

    category_board/post_detail view, templates

--- project/board/urls.py
+++ project/board/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from django.conf import settings
 from board import views
 
+app_name = 'board'
+
 urlpatterns = [
     path('', views.main_board, name='main_board'),
-    # path('<str:category>/', views.univ, name='category_board'),
-    # path('<str:category>/new', views.create_Post, name="create_Post"),
+    path('<str:category_name>/', views.category_board, name='category_board'),
+    path('<str:category_name>/<int:post_pk>', views.post_detail, name='post_detail'),
     # path('<str:category>/<int:pk>/new', views.create_Comment, name="create_Comment"),
     # path('<str:category>/<int:pk>/', views.read_Post, name="read_Post"),
     # path('<str:category>/', views.read_Board, name="read_Board"),
--- project/board/views.py
+++ project/board/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 
 from board.models import Category, Post
 from core.models import Univ
 
 
 def main_board(request, url_name):
+    if request.user.is_authenticated and url_name != request.user.univ.url_name:
+        return redirect('board:main_board', request.user.univ.url_name)
+
     univ = get_object_or_404(Univ, url_name=url_name)
     categories = get_list_or_404(Category, univ=univ)
     posts = Post.objects.filter(ctgy__univ=univ)
@@ -12,4 +16,35 @@ def main_board(request, url_name):
         'univ': univ,
         'categories': categories,
         'posts': posts,
+    })
+
+
+@login_required
+def category_board(request, url_name, category_name):
+    if request.user.univ.url_name != url_name:
+        return redirect('board:main_board', [request.user.univ.url_name])
+
+    univ = get_object_or_404(Univ, url_name=url_name)
+    categories = get_list_or_404(Category, univ=univ)
+    selected_category = get_object_or_404(Category, univ=univ, name=category_name)
+    posts = Post.objects.filter(ctgy=selected_category)
+    return render(request, 'board/category_board.html', {
+        'univ': univ,
+        'categories': categories,
+        'selected_category': selected_category,
+        'posts': posts,
+    })
+
+
+@login_required
+def post_detail(request, url_name, category_name, post_pk):
+    if request.user.univ.url_name != url_name:
+        return redirect('board:main_board', [request.user.univ.url_name])
+
+    univ = get_object_or_404(Univ, url_name=url_name)
+    selected_category = get_object_or_404(Category, univ=univ, name=category_name)
+    post = get_object_or_404(Post, ctgy=selected_category, pk=post_pk)
+
+    return render(request, 'board/post_detail.html', {
+        'post': post,
     })
