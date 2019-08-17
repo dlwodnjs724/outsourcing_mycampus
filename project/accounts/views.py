83771cf741cd44fa538b28210e6deb67d6dfd6e6
changhoi <0417kch@naver.com>
2019-08-17 22:48:05 +0900

    modal

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic.base import View
 
+
+def redirect404(request):
+    raise Http404('There is no matching url')
+
+def check_mail(request):
+    return render(request, "accounts/mail.html")
