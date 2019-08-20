f0ae717d4194735f390c94f984a979d82281a22c
changhoi <0417kch@naver.com>
2019-08-21 02:46:30 +0900

    회원가입, 로그인, 로그아웃 기능 구현됨

--- project/api/models.py
+++ project/api/models.py
@@ -15,7 +15,7 @@ class Token(models.Model):
     @property
     def is_expired(self):
         now = arrow.now().timestamp
-        if (now - self.created_at) > 5 * 60:
+        if (now - self.created_at) > 5 * 60 * 60:
             return True
         else:
             return False
