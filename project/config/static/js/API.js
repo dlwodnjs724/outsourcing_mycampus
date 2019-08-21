2caf2ab0d54293b0b0f7bde370890f1988058497
eatnug <wlq4568@gmail.com>
2019-08-21 13:24:59 +0900

    UI 수정

--- project/config/static/js/API.js
+++ project/config/static/js/API.js
@@ -1,8 +1,10 @@
 const sb = new SendBird({ appId: 'A0DB6C4C-7F09-4D64-B43C-0823B2B35256' });
+const chatroom = document.getElementById('chatroom')
 
 const ChannelHandler = new sb.ChannelHandler();
 ChannelHandler.onMessageReceived = function (channel, message) {
     chatroom.innerHTML = chatroom.innerHTML +`${message._sender.userId} : ${message.message}<br>`
+    chatroom.scrollTop = chatroom.scrollHeight;
 };
 
 sb.addChannelHandler(0, ChannelHandler);
