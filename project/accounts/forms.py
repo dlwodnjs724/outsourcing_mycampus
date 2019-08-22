c2c07e5658438de02cd45bee7eccc1e95b558d82
changhoi <0417kch@naver.com>
2019-08-23 03:32:37 +0900

    signup form html 만들었음, js onclick 만들어야 함

--- project/accounts/forms.py
+++ project/accounts/forms.py
@@ -7,6 +7,20 @@ from accounts.models import User
 
 
 class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Username'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Password'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm Password'
+        })
+        self.fields['class_of'].widget.attrs.update({
+            'placeholder': 'ex) 2022'
+        })
 
     class Meta:
         model = User
