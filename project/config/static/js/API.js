0ff8ab6d25a7ca47b859491502ef40508de12510
eatnug <wlq4568@gmail.com>
2019-08-23 03:26:15 +0900

    비동기 동작 처리를 위한 코드 리팩토링, 에러 핸들링

--- project/config/static/js/API.js
+++ project/config/static/js/API.js
@@ -15,27 +15,60 @@ const loadLog = (channel) => {
     log.reverse = false;
     return log
 }
-const findInviter = (channel) => {
-    const result = {}
-    const first = channel.members[0] 
+const findInvitee = (channel) => {
+    const first = channel.members[0]
     const second = channel.members[1]
-    
-    if (channel.inviter.userId == first.userId) {
-        result["inviter"] = first
-        result["invitee"] = second
-    }
-    else{
-        result["inviter"] = second
-        result["invitee"] = first
-    }
-    return result
+
+    if (channel.inviter.userId == first.userId) return second
+    else return first
+}
+
+const loadChatList = (sb, to) => {
+    const list = []
+    const CLQ = sb.GroupChannel.createMyGroupChannelListQuery()
+    CLQ.includeEmpty = true;
+    if (CLQ.hasNext) {
+        CLQ.next(function (channelList, error) {
+            if (error) return;
+            for (c of channelList) {
+                list.push(c.members.filter(v => v.userId != sb.currentUser.userId)[0].userId)
+            }
+            if (to) to.innerHTML = list.map(cur => `<a href="./${cur}">with ${cur}</a><br>`).join('')
+            else return list
+        })
+    };
+}
+
+const leaveChat = (channel) => {
+    return new Promise( (resolve, reject) => {
+        channel.leave(function(response, error) {
+            if (error) return
+            reject('no such user')
+        })
+    })
+}
+
+const openChat = (sb, other) => {
+    const params = new sb.GroupChannelParams();
+    params.addUserIds([other]);
+    if (sb.currentUser.userId == other) throw new Error('recursive')
+    return new Promise(async (resolve, reject) => {
+        sb.GroupChannel.createDistinctChannelIfNotExist(params, async function (groupChannel, error) {
+            if (error) reject(error)
+            if (!groupChannel.isCreated) reject('already exist')
+            const invitee = findInvitee(groupChannel.channel)
+            if (!invitee) {
+                try { await leaveChat(groupChannel.channel) }
+                catch (e) { alert(e) }
+            }
+            resolve('created')
+        })
+    })
 }
 
 const sb = new SendBird({
     appId: 'A0DB6C4C-7F09-4D64-B43C-0823B2B35256'
 });
-
-
 
 const ChannelHandler = new sb.ChannelHandler();
 
@@ -43,17 +76,13 @@ ChannelHandler.onMessageReceived = function (channel, message) {
     try {
         chatroom.innerHTML += strfy(message)
         chatroom.scrollTop = chatroom.scrollHeight;
-    }
-    catch{
+    } catch {
         console.log('got message')
     }
 };
 
 ChannelHandler.onUserReceivedInvitation = function (groupChannel, inviter, invitees) {
     if (sb.currentUser.userId != inviter.userId) {
-        sb.currentUser.createMetaData({
-            [inviter.userId]: groupChannel.url
-        })
         console.log('invited')
     }
 };
