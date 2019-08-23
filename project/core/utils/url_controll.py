ea9aabcbc89ae8db5ab7f329f7e218357fdaec60
changhoi <0417kch@naver.com>
2019-08-24 03:54:07 +0900

    container 조절, mypage 프론트, redirect_with_next util 정의

---
+++ project/core/utils/url_controll.py
@@ -0,0 +1,15 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+def redirect_with_next(to, next, params):
+    """
+    :param to: redirect 할 곳 (namespace)
+    :param permanent:
+    :param next: 처리 이후 이동할 url namespace
+    :param kwargs: to, next의 args를 담을 것
+    {to: [string], next: [string]}
+    :return:
+    """
+    url = reverse(to, args=[*params['to']]) + "?next=" + reverse(next, args=[*params['next']])
+    return redirect(url)
