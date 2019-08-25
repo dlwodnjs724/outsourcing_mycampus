fa9b803e958aec8a95269f8335a960a2a1800e34
eatnug <wlq4568@gmail.com>
2019-08-25 18:41:49 +0900

    익명채팅 유저 키값 추가, 여전히 unstable

--- project/chat/static/js/chatUtil.js
+++ project/chat/static/js/chatUtil.js
@@ -4,7 +4,7 @@
  */
 const strfy = (message, flag) => {
     if (message._sender.userId == sb.currentUser.userId) return `you : ${message.message}<br>`
-    else if (flag == "anon") return `anon : ${message.message}<br>`
+    else if (flag == "anon") return `anon_${message._sender.metaData.anonKey} : ${message.message}<br>`
     return `${message._sender.userId} : ${message.message}<br>`
 }
 /**
@@ -28,21 +28,14 @@ const loadLog = (url, length, to) => {
     })
 }
 
-const setBase = async (chat_list, chat_header, chat_room) => {
-    const buttons = [...chat_list.querySelectorAll('button')]
-    buttons.forEach(cur => {
-        setChannelBtn(cur, chat_header, chat_room)
-    })
-}
-
 const addChannelBtn = (channel, to, flag) => {
-    const _with = channel.members.filter(v => v.userId != sb.currentUser.userId)[0].userId
-    to.innerHTML += `<button class="url" url="${channel.url}" with="${_with}" flag="${flag}">with ${flag=="norm" ? _with : "anon"}</button><br>`
+    const _with = channel.members.filter(v => v.userId != sb.currentUser.userId)[0]
+    to.innerHTML += `<button class="url" url="${channel.url}" with="${_with.userId}" flag="${flag}" anonKey="${_with.metaData.anonKey}">with ${flag=="norm" ? _with.userId : `anon_${_with.metaData.anonKey}`}</button><br>`
 }
 
 const setChannelBtn = async (button, chat_header, chat_room) => {
     button.addEventListener('click', async e => {
-        chat_header.innerHTML = `chat with ${button.getAttribute('flag')=="norm" ? button.getAttribute('with') : button.getAttribute('flag') }`
+        chat_header.innerHTML = `chat with ${button.getAttribute('flag')=="norm" ? button.getAttribute('with') : button.getAttribute('flag')+'_'+button.getAttribute('anonKey') }`
         chat_room.setAttribute('url', button.getAttribute('url'))
         chat_room.setAttribute('with', button.getAttribute('with'))
         chat_room.setAttribute('flag', button.getAttribute('flag'))
@@ -68,7 +61,7 @@ const loadChatList = async () => {
     };
 }
 
-const C_C = (other, type) => {
+const customCreateChannel = (other, type) => {
     return new Promise( (res, rej) => {
         sb.GroupChannel.createChannelWithUserIds([other], DISTINCT = false, CUSTOM_TYPE = type, function (groupChannel, error) {
             if (error) console.log(error)
@@ -85,7 +78,7 @@ const openAChat = async (other, type) => {
     })
     if (targets.length == 2) throw new Error('already 2 chat')
     else if (targets.length == 1 && targets[0].customType == type) throw new Error(`already existing ${type} chat`)
-    const ret =  await C_C(other, type)
+    const ret =  await customCreateChannel(other, type)
     console.log(ret)
     return ret
 }
