2cd812fc450d74313bc93277c58dd6a2210d6921
이재원 <dlwodnjs724@naver.com>
2019-08-27 16:18:13 +0900

    landing page done

--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from core.forms import UnivRegisterForm
+from core.models import Univ
 
 
 def main(request):
-    return render(request, 'core/main.html')
+    univs = Univ.objects.all()
+
+    if request.method == 'POST':
+        univ_url_name = request.POST.get('univ')
+        if univ_url_name == 'none':
+            return redirect('main_register')
+        if univ_url_name:
+            return redirect('core:board:main_board', univ_url_name)
+
+    return render(request, 'core/main.html', {
+        'univs': univs,
+    })
+
+
+def main_register(request):
+    form = UnivRegisterForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            univ = form.save()
+            return render(request, 'core/success.html', {
+                'univ_name': univ.full_name,
+            })
+
+    return render(request, 'core/register.html', {
+        'form': form,
+    })
