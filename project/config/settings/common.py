47d38a5fbbbfd6dc76aafb8c2497b727bfc9e660
이재원 <dlwodnjs724@naver.com>
2019-08-28 17:31:10 +0900

    change language

--- project/config/settings/common.py
+++ project/config/settings/common.py
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Seoul'
 
