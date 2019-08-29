7c408fe1bf6b940dfa04e2c733d9e49e14c78f6a
happysmileboy <t35114105@gmail.com>
2019-08-30 02:51:16 +0900

    mysql설정

--- project/config/settings/prod.py
+++ project/config/settings/prod.py
@@ -7,10 +7,10 @@ ALLOWED_HOSTS = ['54.180.105.194']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'mycampus-cluster.cluster-ch0ryqkshdif.ap-northeast-2.rds.amazonaws.com',
+        'HOST': 'localhost',
         'PORT': '3306',
-        'NAME': 'mycampus-cluster',
-        'USER': 'admin',
-        'PASSWORD': get_secret("mysql_PASSWORD"),
+        'NAME': 'mycampus',
+        'USER': 'mycampus',
+        'PASSWORD': 'mycampus123',
     }
 }
