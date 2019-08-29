d8fa1b7468f1c6fe0ade38b875d92c257855015e
happysmileboy <t35114105@gmail.com>
2019-08-30 02:18:27 +0900

    mysql설정

--- project/config/settings/prod.py
+++ project/config/settings/prod.py
@@ -4,13 +4,13 @@ from .common import *
 DEBUG = False
 ALLOWED_HOSTS = ['54.180.105.194']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': 'che1-db.czxnimwgemge.ap-northeast-2.rds.amazonaws.com',
-#         'PORT': '3306',
-#         'NAME': 'deploy',
-#         'USER': 'Che1',
-#         'PASSWORD': get_secret("PASSWORD"),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'mycampus-cluster.cluster-ch0ryqkshdif.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'NAME': 'mycampus-cluster',
+        'USER': 'admin',
+        'PASSWORD': get_secret("mysql_PASSWORD"),
+    }
+}
