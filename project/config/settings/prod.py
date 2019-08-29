451b4ca139b919cd609cdd04e6e51d232209faec
happysmileboy <t35114105@gmail.com>
2019-08-30 01:26:32 +0900

    브렌치 생성

--- project/config/settings/prod.py
+++ project/config/settings/prod.py
@@ -2,4 +2,15 @@ from .common import *
 
 
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.180.105.194']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': 'che1-db.czxnimwgemge.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '3306',
+#         'NAME': 'deploy',
+#         'USER': 'Che1',
+#         'PASSWORD': get_secret("PASSWORD"),
+#     }
+# }
